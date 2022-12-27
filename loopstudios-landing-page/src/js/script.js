4acf6527cbea550a02f34e63469c82d444a0b49c
Davide Di Francesco <davide@macbook.local>
2022-12-27 10:49:15 +0100

    loopstudios

---
+++ loopstudios-landing-page/src/js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+const btn = document.getElementById('menu-btn'); 
+const menu = document.getElementById('menu');
+
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+    btn.classList.toggle('open');
+    menu.classList.toggle('hidden');
+}
